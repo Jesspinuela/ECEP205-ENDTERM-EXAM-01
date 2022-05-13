8de685f12c14bc23e1fb0c9f61e284e0ff6e10ca
Jesserie <pinuelajesserie@yahoo.com>
2022-05-13 12:39:48 +0800

    pinuela

---
+++ Item.java
@@ -0,0 +1,47 @@
+package com.ecpe205;
+
+public class Item {
+    private Product product;
+    private double quantity, amount;
+
+    public Item(Product product, double quantity) {
+        this.product = product;
+        this.quantity = quantity;
+
+        //1. Compute for the amount based on the price of the product multiply with quantity
+        //Edit starts here
+        this.amount = getProduct().getPrice() * this.quantity;
+        //Edit ends here
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+        Order order = new Order();
+        //2. Compute amount when the quantity changes
+        //Edit starts here
+        this.amount =  getQuantity() - order.getTotalQuantity();
+        //Edit ends here
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+        Order order = new Order();
+        //3. Compute amount when the quantity changes
+        //Edit starts here
+        this.amount =  this.quantity - order.getTotalQuantity();
+        //Edit ends here
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+}
---
+++ Order.java
@@ -0,0 +1,61 @@
+package com.ecpe205;
+
+import java.util.ArrayList;
+
+public class Order {
+    private ArrayList<Item> items;
+
+    public Order() {
+        this.items = new ArrayList<Item>();
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    /**
+     * Create a new item and append to the items list
+     * @param product
+     * @param quantity
+     */
+    public void addItem(Product product, double quantity) {
+        //edit starts here
+        Item newProduct = new Item(new Product("111", "banana", 5.0), 5);
+        items.add(newProduct);
+
+        //edit ends here
+    }
+
+
+    /**
+     * Remove an item from a given index.
+     * @param index
+     */
+    public void removeItem(int index){
+        //edit starts here
+        items.remove(index);
+        //edit ends here
+    }
+
+
+    /**
+     * Return the total quantity within an order
+     * @return Total Quantity
+     */
+    public double getTotalQuantity() {
+        //edit starts here
+        return getTotalQuantity(); //please remove
+        //edit ends here
+    }
+
+    /**
+     * Returns the overall total of all the items
+     * @return Overall Total
+     */
+    public double getGrandTotal() {
+        //edit starts here
+        double value = items.size();
+        return value; //please remove
+        //edit ends here
+    }
+}
